486374c27e645ae1f4e54a09171b011d58decdb0
Juan Carlos Hernández <jchernandez0312@gmail.com>
2021-07-01 18:03:40 -0500

    :sparkles: Edited frontend
    :sparkles: Edited frontend

--- kmeans/src/App.vue
+++ kmeans/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <h1>Feminicidio en el Perú con kmeans</h1>
     <b-tabs content-class="mt-3">
       <b-tab title="Dataset" active>
         <p v-for="(item, index) in grupos" v-bind:key="index">
@@ -46,21 +47,69 @@
         </table>
       </b-tab>
       <b-tab title="Algoritmo">
-        <div>Kmeans Algorithm</div>
+        <div>Feminicidio en el Perú con KMeans</div>
         <b-form-group
           id="input-group-1"
-          label="Ingrese K"
+          label="Ingrese Datos a predecir"
           label-for="input-1"
           description="K representa el numero de clusters a agrupar"
         >
           <b-form-input
             id="input-1"
-            v-model="cluster"
+            v-model="mes"
+            placeholder="0 - Enero, 12 - Diciembre"
+            required
+          ></b-form-input>
+          <b-form-input
+            id="input-1"
+            v-model="edadVictim"
+            placeholder="ej: 35"
+            required
+          ></b-form-input>
+          <b-form-input
+            id="input-1"
+            v-model="hijosVictim"
             placeholder="ej: 3"
             required
           ></b-form-input>
+          <b-form-input
+            id="input-1"
+            v-model="embaVictim"
+            placeholder="0 - No Embarazada, 1 - Embarazada"
+            required
+          ></b-form-input>
+          <b-form-input
+            id="input-1"
+            v-model="edadAgresor"
+            placeholder="ej: 38"
+            required
+          ></b-form-input>
+          <b-form-input
+            id="input-1"
+            v-model="drogasAlcoh"
+            placeholder="0 - No drogas o alcohol en feminicidio, 1 - Si"
+            required
+          ></b-form-input>
+          <b-form-input
+            id="input-1"
+            v-model="trabajaAgresor"
+            placeholder="0 - No trabaja, 1 - Trabaja"
+            required
+          ></b-form-input>
+          <b-form-input
+            id="input-1"
+            v-model="medidasTomadas"
+            placeholder="ej: 4"
+            required
+          ></b-form-input>
+          <b-form-input
+            id="input-1"
+            v-model="situacionAgresor"
+            placeholder="ej: 1"
+            required
+          ></b-form-input>
         </b-form-group>
-        <b-button class="mr-1" @click="algoritmo">Empezar KMeans</b-button>
+        <b-button class="mr-1" @click="algoritmo">Predecir a que grupo pertenece</b-button>
         <b-button class="mr-1" @click="reset">Reset</b-button>
         <p v-for="(item, index) in clusters" v-bind:key="index">
           El dato {{ index }} pertenece al cluster {{ item }}
@@ -77,12 +126,25 @@ export default {
   data: () => ({
     datos: [],
     clusters: [],
-    cluster: "",
+    mes: null,
+    edadVictim: null,
+    hijosVictim: null,
+    embaVictim: null,
+    edadAgresor: null,
+    drogasAlcoh: null,
+    trabajaAgresor: null,
+    medidasTomadas: null,
+    situacionAgresor: null,
     grupos: [],
+    centroids: [],
   }),
   mounted() {
     axios.get("http://localhost:9000/listar").then((response) => {
       this.datos = response.data;
+      //console.log(this.datos);
+    });
+     axios.get("http://localhost:9000/centroids").then((response) => {
+      this.centroids = response.centroids;
       //console.log(this.datos);
     });
   },
@@ -116,11 +178,15 @@ export default {
   text-align: center;
   align-content: center;
   justify-content: center;
-  display: flex;
   color: #2c3e50;
   margin: 60px;
+  width: 100%;
 }
 .buton {
   margin: 10;
 }
+b-tab{
+  width: 100%;
+}
+
 </style>
